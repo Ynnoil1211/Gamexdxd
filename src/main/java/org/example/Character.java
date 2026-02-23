f281cff27b1793471e21014b50ea45e99a8f61d1
Ynnoil1211 <lionnylinli666@gmail.com>
2026-02-23 14:14:11 -0500

    Refactor Character attributes.

--- src/main/java/org/example/Character.java
+++ src/main/java/org/example/Character.java
@@ -6,77 +6,42 @@ interface Purchasable {
 }
 public abstract class Character implements Purchasable {
     private String name;
-    private int actuHp;
-    private int maxiHp;
-    private int basePw;
-    private int baseMagicPw;
-    private int baseDefense;
-    private int baseMagicDefense;
+    private double actuHp;
+    private double maxiHp;
+    private double basePw;
+    private double baseMagicPw;
+    private double baseDefense;
+    private double baseMagicDefense;
     private double criticChance;
     private double dodge;
     private int price;
     private Equipment equip;
-
     private double speed;
+
     private boolean isEnemy;
     public Character(){}
-    public Character(String name, int maxiHp, int basePw, Equipment equip, int price, double speed){
+    public Character(String name, double maxiHp, double basePw, double baseMagicPw, double baseDefense, double baseMagicDefense, double criticChance, double speed, double dodge, int price, Equipment equip){
         this.name = name;
         this.actuHp = maxiHp;
         this.maxiHp = maxiHp;
         this.basePw = basePw;
-        this.equip = equip;
-        this.price = price;
+        this.baseMagicPw = baseMagicPw;
+        this.baseDefense = baseDefense;
+        this.baseMagicDefense = baseMagicDefense;
+        this.criticChance = criticChance;
         this.speed = speed;
+        this.dodge = dodge;
+        this.price = price;
+        this.equip = equip;
         this.isEnemy = false;
     }
-
-    public double getSpeed() {
-        return speed;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public int getBasePw() {
-        return basePw;
-    }
-    public void setBasePw(int basePw) {
-        this.basePw = basePw;
-    }
-    public int getMaxiHp() {
-        return maxiHp;
-    }
-    public void setMaxiHp(int maxiHp) {
-        this.maxiHp = maxiHp;
-    }
-    public int getActuHp() {
-        return actuHp;
-    }
-    public Equipment getEquip() {
-        return equip;
-    }
-    public void setEquip(Equipment equip) {
-        this.equip = equip;
-    }
-
-    public void setActuHp(int actuHp) {
-        if (actuHp > this.maxiHp) {
-            this.actuHp = this.maxiHp;
-        } else {
-            this.actuHp = actuHp;
-        }
-    }
-
     public boolean isAlive(){
         return getActuHp()>0;
     }
 
     public abstract void attack(Character enem);
 
-    public void reciDmg(int dmg){
+    public void reciDmg(double dmg){
         this.actuHp -= dmg;
         if(!isAlive()) actuHp = 0;
     }
@@ -87,37 +52,103 @@ public abstract class Character implements Purchasable {
 
     public abstract Character clon();
 
+    public double getActuHp() {
+        return actuHp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBaseMagicPw() {
+        return baseMagicPw;
+    }
+
+    public double getMaxiHp() {
+        return maxiHp;
+    }
+
+    public double getBasePw() {
+        return basePw;
+    }
+
+    public double getBaseDefense() {
+        return baseDefense;
+    }
+
+    public double getBaseMagicDefense() {
+        return baseMagicDefense;
+    }
+
+    public double getCriticChance() {
+        return criticChance;
+    }
+
+    public double getDodge() {
+        return dodge;
+    }
+
+    public Equipment getEquip() {
+        return equip;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public boolean isEnemy() {
+        return isEnemy;
+    }
 }
 class RageWarrior extends Character{
     private int rage;
     private boolean bonusHit = false;
     //base datas for random
-     private static final int randHp = 500;
-     private static final int randPw = 125;
-     private static final int randPrice = 300;
-     private static final int randSpeed = 12;
-
+    private static final double randHp = 500;
+    private static final double randBasePw = 100;
+    private static final double randMagicPw = 0;
+    private static final double randBaseDefence = 150;
+    private static final double randMagicDefense = 125;
+    private static final double randCriticChance = 0.025;
+    private static final double randSpeed = 12;
+    private static final double randDodge = 0.05;
+    private static final int randPrice = 300;
     public RageWarrior(){}
     public RageWarrior(String name, Equipment equip){
         Random rand = new Random();
-        int hpVariance = (int) (randHp * 0.10);
-        int maxiHp = randHp + (rand.nextInt((hpVariance * 2) + 1) - hpVariance);
+        double hpVariance = (double) (randHp * 0.15);
+        double maxiHp = randHp + (rand.nextDouble((hpVariance * 2) + 1) - hpVariance);
 
-        int pwVariance = (int) (125 * 0.15);
-        int basePw = 125 + (rand.nextInt((pwVariance * 2) + 1) - pwVariance);
+        double pwVariance = (double) (randBasePw * 0.15);
+        double basePw = randBasePw + (rand.nextDouble((pwVariance * 2) + 1) - pwVariance);
 
-        int priceVariance = (int) (300 * 0.15);
-        int price = 300 + (rand.nextInt((priceVariance * 2) + 1) - priceVariance);
+        double magicPwVariance = (double) (randMagicPw * 0.15);
+        double baseMagicPw = randMagicPw + (rand.nextDouble((magicPwVariance * 2) + 1) - magicPwVariance);
 
-        double speedVariance = (double) (12 * 0.20);
-        double speed = 12 + (rand.nextDouble((speedVariance * 2) + 1) - speedVariance);
+        double defenseVariance = (double) (randBaseDefence * 0.15);
+        double baseDefense = randBaseDefence + (rand.nextDouble((defenseVariance * 2) + 1) - defenseVariance);
 
-        super(name, maxiHp, basePw, equip, price, speed);
+        double magicDefenseVariance = (double) (randMagicDefense * 0.15);
+        double baseMagicDefense = randMagicDefense + (rand.nextDouble((magicDefenseVariance * 2) + 1) - magicDefenseVariance);
+
+        double criticVariance = (double) (randCriticChance * 0.15);
+        double criticChance = randCriticChance + (rand.nextDouble((criticVariance * 2) + 1) - criticVariance);
+
+        double speedVariance = (double) (randSpeed * 0.15);
+        double speed = randSpeed + (rand.nextDouble((speedVariance * 2) + 1) - speedVariance);
+
+        double dodgeVariance = (double) (randDodge * 0.20);
+        double dodge = randDodge + (rand.nextDouble((speedVariance * 2) + 1) - speedVariance);
+
+        int priceVariance = (int) (randPrice * 0.15);
+        int price = randPrice + (rand.nextInt((priceVariance * 2) + 1) - priceVariance);
+
+        super(name, maxiHp, basePw, baseMagicPw, baseDefense, baseMagicDefense, criticChance, speed, dodge, price, equip);
         this.rage = 0;
     }
 
-    public RageWarrior(String name, int maxiHp, int basePw, Equipment equipmentCloned, int price, double speed) {
-        super(name, maxiHp, basePw, equipmentCloned, price, speed);
+    public RageWarrior(String name, double maxiHp, double basePw, double baseMagicPw, double baseDefense, double baseMagicDefense, double criticChance, double speed, double dodge, int price, Equipment equip) {
+        super(name, maxiHp, basePw, baseMagicPw, baseDefense, baseMagicDefense, criticChance, speed, dodge, price, equip);
         this.rage = 0;
     }
 
@@ -136,22 +167,19 @@ class RageWarrior extends Character{
             equipmentCloned = this.getEquip().clon();
         }
         return new RageWarrior(
-                this.getName(),
-                this.getMaxiHp(),
-                this.getBasePw(),
-                equipmentCloned,
-                this.getPrice(),
-                this.getSpeed()
-        );
+                this.getName(), this.getMaxiHp(), this.getBasePw(),
+                this.getBaseMagicPw(), this.getBaseDefense(), this.getBaseMagicDefense(),
+                this.getCriticChance(), this.getSpeed(),
+                this.getDodge(), this.getPrice(), equipmentCloned);
     }
 
     @Override
     public void attack(Character enem) {
-        int bonusDmg = getEquip().getBonusDmg();
+        double bonusDmg = getEquip().getBonusDmg();
         System.out.println("BASE POWER = " + getBasePw());
         System.out.println("BONUS DMG = " + bonusDmg);
         System.out.println("ACTUAL RAGE = " + getRage());
-        int totalDmg = getBasePw() + bonusDmg;
+        double totalDmg = getBasePw() + bonusDmg;
         System.out.println("TOTAL DMG = " + totalDmg);
         if(bonusHit){
             System.out.println("RAGE HIT! ");
@@ -176,40 +204,53 @@ class RageWarrior extends Character{
 }
 class MageWarrior extends Character{
     private int actuMana;
-    private static final int randHp = 300;
-    private static final int randPw = 100;
-    private static final int randPrice = 350;
-    private static final int randSpeed = 15;
-    public MageWarrior(){}
+    private static final double randHp = 500;
+    private static final double randBasePw = 100;
+    private static final double randMagicPw = 0;
+    private static final double randBaseDefence = 150;
+    private static final double randMagicDefense = 125;
+    private static final double randCriticChance = 0.025;
+    private static final double randSpeed = 12;
+    private static final double randDodge = 0.05;
+    private static final int randPrice = 300;
 
+    public MageWarrior(){}
     public MageWarrior(String name, Equipment equip){
         Random rand = new Random();
-        int hpVariance = (int) (200 * 0.10);
-        int maxiHp = 200 + (rand.nextInt((hpVariance * 2) + 1) - hpVariance);
+        double hpVariance = (double) (randHp * 0.15);
+        double maxiHp = randHp + (rand.nextDouble((hpVariance * 2) + 1) - hpVariance);
 
-        int pwVariance = (int) (125 * 0.15);
-        int basePw = 125 + (rand.nextInt((pwVariance * 2) + 1) - pwVariance);
+        double pwVariance = (double) (randBasePw * 0.15);
+        double basePw = randBasePw + (rand.nextDouble((pwVariance * 2) + 1) - pwVariance);
 
-        int priceVariance = (int) (300 * 0.15);
-        int price = 300 + (rand.nextInt((priceVariance * 2) + 1) - priceVariance);
+        double magicPwVariance = (double) (randMagicPw * 0.15);
+        double baseMagicPw = randMagicPw + (rand.nextDouble((magicPwVariance * 2) + 1) - magicPwVariance);
 
-        double speedVariance = (double) (12 * 0.20);
-        double speed = 12 + (rand.nextDouble((speedVariance * 2) + 1) - speedVariance);
+        double defenseVariance = (double) (randBaseDefence * 0.15);
+        double baseDefense = randBaseDefence + (rand.nextDouble((defenseVariance * 2) + 1) - defenseVariance);
 
-        super(name, maxiHp, basePw, equip, price, speed);
+        double magicDefenseVariance = (double) (randMagicDefense * 0.15);
+        double baseMagicDefense = randMagicDefense + (rand.nextDouble((magicDefenseVariance * 2) + 1) - magicDefenseVariance);
+
+        double criticVariance = (double) (randCriticChance * 0.15);
+        double criticChance = randCriticChance + (rand.nextDouble((criticVariance * 2) + 1) - criticVariance);
+
+        double speedVariance = (double) (randSpeed * 0.15);
+        double speed = randSpeed + (rand.nextDouble((speedVariance * 2) + 1) - speedVariance);
+
+        double dodgeVariance = (double) (randDodge * 0.20);
+        double dodge = randDodge + (rand.nextDouble((speedVariance * 2) + 1) - speedVariance);
+
+        int priceVariance = (int) (randPrice * 0.15);
+        int price = randPrice + (rand.nextInt((priceVariance * 2) + 1) - priceVariance);
+
+        super(name, maxiHp, basePw, baseMagicPw, baseDefense, baseMagicDefense, criticChance, speed, dodge, price, equip);
+        this.actuMana = 100;
     }
 
-    public MageWarrior(String name, int maxiHp, int basePw, int actuMana, Equipment equip, int price, double speed){
-        super(name,  maxiHp,  basePw,  equip, price, speed);
-        this.actuMana = actuMana;
-    }
-
-    public int getActuMana() {
-        return actuMana;
-    }
-
-    public void setActuMana(int actuMana) {
-        this.actuMana = actuMana;
+    public MageWarrior(String name, double maxiHp, double basePw, double baseMagicPw, double baseDefense, double baseMagicDefense, double criticChance, double speed, double dodge, int price, Equipment equip) {
+        super(name, maxiHp, basePw, baseMagicPw, baseDefense, baseMagicDefense, criticChance, speed, dodge, price, equip);
+        this.actuMana = 100;
     }
 
     @Override
@@ -219,23 +260,19 @@ class MageWarrior extends Character{
             equipmentCloned = this.getEquip().clon();
         }
         return new MageWarrior(
-                this.getName(),
-                this.getMaxiHp(),
-                this.getBasePw(),
-                this.getActuMana(),
-                equipmentCloned,
-                this.getPrice(),
-                this.getSpeed()
-        );
+                this.getName(), this.getMaxiHp(), this.getBasePw(),
+                this.getBaseMagicPw(), this.getBaseDefense(), this.getBaseMagicDefense(),
+                this.getCriticChance(), this.getSpeed(),
+                this.getDodge(), this.getPrice(), equipmentCloned);
     }
 
     @Override
     public void attack(Character enem) {
-        int bonusDmg = getEquip().getBonusDmg();
+        double bonusDmg = getEquip().getBonusDmg();
         System.out.println("BASE POWER = " + getBasePw());
         System.out.println("BONUS DMG = " + bonusDmg);
         System.out.println("ACTUAL MANA = " + getActuMana());
-        int totalDmg = getBasePw() + bonusDmg;
+        double totalDmg = getBasePw() + bonusDmg;
         if(getActuMana()<0){
             System.out.println("ACTUAL MANA IS LOWER THAN 0, NO BONUS DMG DEALER.");
             totalDmg = getBasePw();
@@ -249,5 +286,13 @@ class MageWarrior extends Character{
         } else{
             System.out.println("ENEM. ACTUAL HP AFTER DMG = " + enem.getActuHp());
         }
+    }
+
+    public int getActuMana() {
+        return actuMana;
+    }
+
+    public void setActuMana(int actuMana) {
+        this.actuMana = actuMana;
     }
 }
